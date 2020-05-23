0a23c0e8612ac23b35997a7237a99e67eec8b04e
Tommy Dwi P <tommy.putranto@amarbank.co.id>
2020-05-23 12:34:04 +0700

    /db

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-    "bytes"
+	"bytes"
+	"fmt"
+	"database/sql"
     "log"
     "net/http"
     "os"
@@ -9,7 +11,7 @@ import (
 	
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
-    _ "github.com/heroku/x/hmetrics/onload"
+    _ "github.com/lib/pq"
     "github.com/russross/blackfriday"
 )
 
@@ -21,6 +23,45 @@ func repeatHandler(r int) gin.HandlerFunc {
             buffer.WriteString("Hello from Go!\n")
         }
         c.String(http.StatusOK, buffer.String())
+    }
+}
+
+func dbFunc(db *sql.DB) gin.HandlerFunc {
+    return func(c *gin.Context) {
+        if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+            c.String(http.StatusInternalServerError,
+                fmt.Sprintf("Error creating database table: %q", err))
+            return
+        }
+
+        if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+            c.String(http.StatusInternalServerError,
+                fmt.Sprintf("Error incrementing tick: %q", err))
+            return
+        }
+
+        rows, err := db.Query("SELECT tick FROM ticks")
+        if err != nil {
+            c.String(http.StatusInternalServerError,
+                fmt.Sprintf("Error reading ticks: %q", err))
+            return
+		}
+
+		db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+		if err != nil {
+			log.Fatalf("Error opening database: %q", err)
+		}
+	
+        defer rows.Close()
+        for rows.Next() {
+            var tick time.Time
+            if err := rows.Scan(&tick); err != nil {
+                c.String(http.StatusInternalServerError,
+                    fmt.Sprintf("Error scanning ticks: %q", err))
+                return
+            }
+            c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+        }
     }
 }
 
@@ -53,5 +94,7 @@ func main() {
 	
 	router.GET("/repeat", repeatHandler(repeat))
 	
+	router.GET("/db", dbFunc(db))
+
 	router.Run(":" + port)
 }
