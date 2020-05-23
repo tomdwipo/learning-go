91a78ebfea66275ecd3cdb4786b6f5d8240f78dc
Tommy Dwi P <tommy.putranto@amarbank.co.id>
2020-05-23 12:09:47 +0700

    Markdown demo dependency

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+    _ "github.com/heroku/x/hmetrics/onload"
+    "github.com/russross/blackfriday"
 )
 
 func main() {
@@ -24,6 +26,10 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
+
+	router.GET("/mark", func(c *gin.Context) {
+		c.String(http.StatusOK, string(blackfriday.Run([]byte("**hi!**"))))
+	  })
 
 	router.Run(":" + port)
 }
